bfae308ded5d5822d28a46db1153a397a0a17c71
Anton064 <63185453+Anton064@users.noreply.github.com>
2020-05-09 15:53:11 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+red='033[1;31m'
+white='033[1;37m'
+green='033[1;32m'
+
+printf "$green"
+echo "UPDATE"
+apt update && apt upgrade
+pkg install git
+pkg install wget
+pkg install ssh
+pkg install Openssh
+pkg install php
+pkg install root-repo
+pkg install 11x-repo
+pkg install unstable-repo
+pkg install apache2
+pkg install beanshell
+pkg install binutils
+pkg install busybox
+pkg install coreutils
+pkg install cscope
+pkg install mtools
+pkg install root-repo repository
+pkg install virustotal-cli
+pkg install wget
+pkg install mtools
+
+printf "$red"
+echo "Update completed"
